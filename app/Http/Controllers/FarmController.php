f2e504e4bbaf2296d3708c8ede7d9f0be719f81b
mervins <mersongwapo@gmail.com>
2022-11-22 21:00:37 +0800

    update farm controller

--- app/Http/Controllers/FarmController.php
+++ app/Http/Controllers/FarmController.php
@@ -53,7 +53,8 @@ class FarmController extends Controller
         Farm::create([
             'farmer_id' => $request->selected_farmer,
             'income' => 0,
-            'details' => $request->details
+            'details' => $request->details, 
+            'map' => $request->map
         ]);
 
         return Redirect::back();
@@ -94,7 +95,8 @@ class FarmController extends Controller
         $farm->update([
             'map_id' => $request->map_id,
             'farmer_id' => $request->farmer_id,
-            'details' => $request->details
+            'details' => $request->details,
+            'map' => $request->map
         ]);
 
         return Redirect::back();
