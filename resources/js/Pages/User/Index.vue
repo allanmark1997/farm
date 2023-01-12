1833da83583ad94d07aa5e97a309848094569f2b
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-01-12 21:59:18 +0800

    added table design in user manangement

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -167,7 +167,7 @@ const toggle_status = (user) => {
                     </div>
                     <table class="w-full text-sm text-left text-gray-500">
                         <thead
-                            class="text-xs text-gray-700 uppercase bg-gray-50"
+                            class="text-xs text-gray-700 uppercase bg-green-300"
                         >
                             <tr>
                                 <th scope="col" class="px-6 py-3">Name</th>
