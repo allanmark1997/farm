9090c75dd6b066a62b242a9d414d28a1e23099d8
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-01-18 12:32:08 +0800

    update add farmer

--- resources/js/Pages/Farmer/AddFarmer.vue
+++ resources/js/Pages/Farmer/AddFarmer.vue
@@ -714,6 +714,40 @@ Number of living household members"
                             />
                         </div>
                     </div>
+                    <hr />
+
+                    <div class="flex m-2 mt-2">
+                        <div class="mr-2 w-1/2">
+                            <label
+                                for="Number of Parcels"
+                                class="block mb-2 text-sm font-medium text-gray-900"
+                                >Number of Parcels</label
+                            >
+                            <input
+                                type="text"
+                                name="number_of_parcels"
+                                id="number_of_parcels"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                placeholder="Number of Parcels"
+                                required=""
+                            />
+                        </div>
+                        <div class="mr-2 w-1/2">
+                            <label
+                                for="Agrarian Reform Beneficiary (ARB)"
+                                class="block mb-2 text-sm font-medium text-gray-900"
+                                >Agrarian Reform Beneficiary (ARB)</label
+                            >
+                            <select
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                name=""
+                                id=""
+                            >
+                                <option value="0">Yes</option>
+                                <option value="1">No</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
